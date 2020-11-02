da4c1004bf8edb000a2fbc2229f272ee85d2ac7d
jdquerales <jquerales72@gmail.com>
2020-11-02 23:00:46 +0000

    Add link to JQuery on the head & create carrousel effect for home background images.

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+ $( document ).ready(function() {
+
+  var images = [
+
+  'assets/img/luggage.jpg',
+  'assets/img/maldives.jpg',
+  'assets/img/eiffel-tower.jpg'
+
+];
+
+
+var index = 0;
+
+setInterval(change_up, 5000);
+
+function change_up(){
+
+  index = (index + 1 < images.length) ? index + 1 : 0;
+
+  $('#home').fadeOut(5000, function(){
+
+    $(this).css('background-image', 'url('+ images[index] + ')')
+
+    $(this).fadeIn(5000);
+
+  });
+}
+
+});
+
+
