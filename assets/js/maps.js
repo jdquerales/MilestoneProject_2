14f5c9832b6cb59c98967dc26b4c49160f8d8e80
jdquerales <jquerales72@gmail.com>
2020-11-13 15:04:53 +0000

    Add customized icons on Google maps in maps.js: pinSymbol() function

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,6 @@
 var map, autocomplete, service, place, mylat, mylng, option;
+const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let labelIndex = 0;
 
 
 function initMap() {
@@ -12,6 +14,12 @@ function initMap() {
   const input = document.getElementById("autocomplete");
 
   autocomplete = new google.maps.places.Autocomplete(input);
+
+
+  const infowindow = new google.maps.InfoWindow();
+  const infowindowContent = document.getElementById("infowindow-content");
+  infowindow.setContent(infowindowContent);
+
   service = new google.maps.places.PlacesService(map);
 
 
@@ -36,6 +44,9 @@ function initMap() {
     // Extract coordinates of the input location
 
     place = autocomplete.getPlace();
+
+    
+
     if ($("#accommodation").is(':selected')) {
         if (place.geometry) {
             map.panTo(place.geometry.location);
@@ -107,6 +118,22 @@ function initMap() {
         }
     }
 
+    else if ($("#shopping").is(':selected')) {
+        if (place.geometry) {
+            map.panTo(place.geometry.location);
+            map.setZoom(14);
+            var search = {
+                bounds: map.getBounds(),
+                types: ['store', 'shopping_mall']
+            };
+            doNearbySearch(search);
+        }
+        else {
+            $('#autocomplete').attr("placeholder", "Enter a town or city");
+        }
+    }
+
+
   }
 
 
@@ -122,19 +149,29 @@ function createMarkers(places, map) {
       anchor: new google.maps.Point(17, 34),
       scaledSize: new google.maps.Size(25, 25),
     };
-    new google.maps.Marker({
-      map,
-      icon: image,
-      title: place.name,
-      position: place.geometry.location,
-      animation: google.maps.Animation.DROP,
+
+    
+    marker = new google.maps.Marker({
+    //  map,
+    //    icon: image,
+        icon : pinSymbol('blue'),
+        title: place.name,
+    //  position: place.geometry.location,
+    animation: google.maps.Animation.DROP,
+    position: place.geometry.location,
+    label: labels[labelIndex++ % labels.length],
+    map: map,
     });
+
+
     const li = document.createElement("li");
-    li.textContent = place.name;
+    li.textContent =marker.label + ") " + place.name; 
     placesList.appendChild(li);
     bounds.extend(place.geometry.location);
   }
   map.fitBounds(bounds);
+
+  
 }
 
 
@@ -172,3 +209,15 @@ function reset() {
 $("#reset").click(function(){
   document.location.reload(true);
 });
+
+function pinSymbol(color) {
+    return {
+        path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z',
+        fillColor: color,
+        fillOpacity: 1,
+        strokeColor: '#000',
+        strokeWeight: 2,
+        scale: 1.2,
+        labelOrigin: new google.maps.Point(0, -30)
+    };
+}
