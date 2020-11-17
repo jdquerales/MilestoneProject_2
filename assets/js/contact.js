35dc9bb538f35da23981f15ef4f0d6b3a9416268
jdquerales <jquerales72@gmail.com>
2020-11-17 15:35:01 +0000

    Add EmailJS JavaScript SDK in contact.html and corresponding function in contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,28 +1,45 @@
- // Example starter JavaScript for disabling form submissions if there are invalid fields
-      (function () {
-        "use strict";
-        window.addEventListener(
-          "load",
-          function () {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName("needs-validation");
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function (
-              form
-            ) {
-              form.addEventListener(
-                "submit",
-                function (event) {
-                  if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                  }
-                  form.classList.add("was-validated");
-                },
-                false
-              );
-            });
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
           },
           false
         );
-      })();
+      });
+    },
+    false
+  );
+})();
+
+
+function sendMail(contactForm) {
+    emailjs.send("gmail.com", "template_bq0n2sx", {
+        "from_name": contactForm.validationCustom01.value,
+        "from_surname": contactForm.validationCustom02.value,
+        "from_email": contactForm.validationCustom03.value,
+        "project_request": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,21 @@
- $( document ).ready(function() {
-
+$(document).ready(function () {
   var images = [
+    "assets/img/luggage.jpg",
+    "assets/img/maldives.jpg",
+    "assets/img/eiffel-tower.jpg",
+  ];
 
-  'assets/img/luggage.jpg',
-  'assets/img/maldives.jpg',
-  'assets/img/eiffel-tower.jpg'
+  var index = 0;
 
-];
+  setInterval(change_up, 5000);
 
+  function change_up() {
+    index = index + 1 < images.length ? index + 1 : 0;
 
-var index = 0;
+    $("#home").fadeOut(5000, function () {
+      $(this).css("background-image", "url(" + images[index] + ")");
 
-setInterval(change_up, 5000);
-
-function change_up(){
-
-  index = (index + 1 < images.length) ? index + 1 : 0;
-
-  $('#home').fadeOut(5000, function(){
-
-    $(this).css('background-image', 'url('+ images[index] + ')')
-
-    $(this).fadeIn(5000);
-
-  });
-}
-
+      $(this).fadeIn(5000);
+    });
+  }
 });
-
-
