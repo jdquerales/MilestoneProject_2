2d1ecd721a01582194475218f289e24858afc63d
jdquerales <jquerales72@gmail.com>
2020-11-18 21:24:59 +0000

    Add links to Google Maps in suggestions.html

--- assets/js/suggestions.js
+++ assets/js/suggestions.js
@@ -1,27 +1,117 @@
-function initMap() {
-    const buenosaires = { lat: -34.6037, lng: -58.3816 }; 
-    const map = new google.maps.Map(
-        document.getElementById("suggestions-map"), {
-            zoom : 12,
-            center : buenosaires,
-        });
-    marker = new google.maps.Marker(
-        {
-        position: buenosaires,
-        map,
-        draggable: true,
-        title: "Buenos Aires",
-        animation: google.maps.Animation.DROP,
-    });
+$(document).ready(function () {
+  $("#paris").on("click", initMapPA);
+});
 
-    marker.addListener("click", toggleBounce);
-    
+$(document).ready(function () {
+  $("#buenos_aires").on("click", initMapBA);
+});
+
+$(document).ready(function () {
+  $("#cancun").on("click", initMapCA);
+});
+
+$(document).ready(function () {
+  $("#cuba").on("click", initMapCU);
+});
+
+var map;
+var coordinates = [
+  {
+    lat: 48.864716,
+    lng: 2.349014,
+  },
+  {
+    lat: -34.6037,
+    lng: -58.3816,
+  },
+  {
+    lat: 21.1619,
+    lng: -86.8515,
+  },
+  {
+    lat: 23.113592,
+    lng: -82.366592,
+  },
+];
+var markers = [];
+
+function initMapPA() {
+  const myLatLng = { lat: 51.8985, lng: -8.4756 };
+  const map = new google.maps.Map(document.getElementById("suggestions-map"), {
+    zoom: 6,
+    center: coordinates[0],
+  });
+
+  var marker = new google.maps.Marker({
+    position: coordinates[0],
+    map: map,
+    animation: google.maps.Animation.DROP,
+  });
+
+  marker.addListener("click", toggleBounce);
+}
+
+function initMapBA() {
+  const myLatLng = { lat: 51.8985, lng: -8.4756 };
+  const map = new google.maps.Map(document.getElementById("suggestions-map"), {
+    zoom: 6,
+    center: coordinates[1],
+  });
+
+  var marker = new google.maps.Marker({
+    position: coordinates[1],
+    map: map,
+    animation: google.maps.Animation.DROP,
+  });
+
+  marker.addListener("click", toggleBounce);
+}
+
+function initMapCA() {
+  const myLatLng = { lat: 51.8985, lng: -8.4756 };
+  const map = new google.maps.Map(document.getElementById("suggestions-map"), {
+    zoom: 6,
+    center: coordinates[2],
+  });
+
+  var marker = new google.maps.Marker({
+    position: coordinates[2],
+    map: map,
+    animation: google.maps.Animation.DROP,
+  });
+
+  marker.addListener("click", toggleBounce);
+}
+
+function initMapCU() {
+  const myLatLng = { lat: 51.8985, lng: -8.4756 };
+  const map = new google.maps.Map(document.getElementById("suggestions-map"), {
+    zoom: 6,
+    center: coordinates[3],
+  });
+
+  var marker = new google.maps.Marker({
+    position: coordinates[3],
+    map: map,
+    animation: google.maps.Animation.DROP,
+  });
+
+  marker.addListener("click", toggleBounce);
 }
 
 function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+function displayMap() {
+  var x = document.getElementById("suggestions-map");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
