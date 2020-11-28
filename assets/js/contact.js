6e25c09ab06a841ed25414bea261b10a7a7f5586
jdquerales <jquerales72@gmail.com>
2020-11-28 22:50:38 +0000

    Fix bug in the contact form: message will be sent only if the content is not empty

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -18,6 +18,8 @@
               alert("Something went wrong!");
             }
             form.classList.add("was-validated");
+            
+
           },
           false
         );
@@ -30,20 +32,28 @@
 /* This function send email through EmailJS service by taking inputs from form in contact.html */
 
 function sendMail(contactForm) {
-  emailjs
-    .send("gmail.com", "template_bq0n2sx", {
+
+console.log(contactForm.message.value.length);
+
+// Here the email will be sent only if the body of the message is not empty:
+
+if(contactForm.message.value.length!=0){
+  emailjs.send("gmail.com", "template_bq0n2sx", {
       from_name: contactForm.validationCustom01.value,
       from_surname: contactForm.validationCustom02.value,
       from_email: contactForm.validationCustom03.value,
       project_request: contactForm.message.value,
-    })
-    .then(
+    }).then(
       function (response) {
         console.log("SUCCESS", response);
       },
       function (error) {
         console.log("FAILED", error);
-      }
+      },
     );
+    alert("Your message has been sent!")
+};
+
   return false; // To block from loading a new page
 }
+
